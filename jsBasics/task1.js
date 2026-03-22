53450bb29cf874ceeb4e9738337ee475dd8499ad
olena2222 <olena.bosa@gmail.com>
2026-03-22 21:24:17 +0200

    added js-basics tasks, lesson4

---
+++ jsBasics/task1.js
@@ -0,0 +1,15 @@
+let valueString = "good afternoon";
+let valueString1 = "good evening";
+let valueString2 = `good morning`;
+let valueNumber = 200;
+let isTrue = true;
+let valueNull = null;
+let valueUndefined = undefined;
+
+console.log(valueString);
+console.log(valueString1);
+console.log(valueString2);
+console.log(valueNumber);
+console.log(isTrue);
+console.log(valueNull);
+console.log(valueUndefined);
---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+const firstPerson = "Ben";
+const secondPerson = "Dan";
+let greeting =
+  "Hello" + " " + firstPerson + " " + "and" + " " + secondPerson + "!";
+console.log(greeting);
+let greeting2 = `Hello ${firstPerson} and ${secondPerson}!`;
+console.log(greeting2);
