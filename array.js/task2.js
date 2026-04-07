33ce5287076939680566fe5a6cd06360ddfa6ecd
unknown <potatoesWithChicken>
2026-04-07 19:17:35 +0200

    added hometask regarding array + improve task1 from previous lesson

---
+++ array.js/task2.js
@@ -0,0 +1,4 @@
+const array = [1, 2, 3, 4, 5];
+const newArray = array.map((value, index) => value * index);
+
+console.log(newArray);
---
+++ array.js/task5.js
@@ -0,0 +1,5 @@
+const firstArray = [1, "two"];
+const secondArray = ["three", 4];
+const newArray = firstArray.concat(secondArray);
+
+console.log(newArray);
